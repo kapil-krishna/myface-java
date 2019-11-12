f13075201cd6d80b93867d9e9c280a086236a22f
Kapil Krishna <kapil.krishna.yo@gmail.com>
2019-11-12 16:32:17 +0000

    got all posts, all users, create user, create post
    create post not working

--- src/main/java/techsmiths/myface/controllers/PostController.java
+++ src/main/java/techsmiths/myface/controllers/PostController.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import techsmiths.myface.models.dbmodels.Post;
+import techsmiths.myface.models.dbmodels.User;
 import techsmiths.myface.models.viewmodels.AllPostsViewModel;
+import techsmiths.myface.models.viewmodels.AllUsersViewModel;
 import techsmiths.myface.services.PostService;
+import techsmiths.myface.services.UserService;
+
 
 import java.util.List;
 
@@ -37,7 +41,13 @@ public class PostController {
 
     @RequestMapping(value = "/create", method = RequestMethod.GET)
     public ModelAndView getCreatePostPage() {
-        return new ModelAndView("posts/createPost");
+
+        UserService userService = new UserService();
+
+        List<User> users = userService.getAllUsers(50, 0);
+        AllUsersViewModel usersViewModel = new AllUsersViewModel(users);
+
+        return new ModelAndView("posts/createPost","model", usersViewModel);
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
